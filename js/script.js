43839ad4f0c5107fd2e109faf999a70122c4c5bc
Eddie <eddie.jostell@gmail.com>
2017-02-21 21:28:56 +0100

    More cosmetics on index.html and some stuff in .js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//Movie Constructor
+function createMovie() {
+
+	const MovieConstructor = function(title, year, genres) {
+		this.tile = tile;
+		this.year = year;
+		this.genres = genres;
+	};
+
+
+//var wardogs = new MovieConstructor("War Dogs", 2016, ["Comedy", " Crime", " Drama"], 90000);
+
+MovieConstructor.prototype.listMovie = function() {
+	//console.log("This is " + wardogs.title + " made in " + wardogs.year + " listed as a " + wardogs.genres + " and it has a rating of " + wardogs.ratings);
+}
+//MovieConstructor.prototype.listMovie();
+
+};
+createMovie();
+
 
 //MovieDatabase Module Pattern
 
@@ -60,28 +80,6 @@ const dataBase = (() => {
 
 
 })();
-
-
-//Movie Constructor
-function createMovie() {
-
-	const MovieConstructor = function(title, year, genres) {
-		this.tile = tile;
-		this.year = year;
-		this.genres = genres;
-	};
-
-
-};
-createMovie();
-
-//var wardogs = new MovieConstructor("War Dogs", 2016, ["Comedy", " Crime", " Drama"], 90000);
-
-MovieConstructor.prototype.listMovie = function() {
-	//console.log("This is " + wardogs.title + " made in " + wardogs.year + " listed as a " + wardogs.genres + " and it has a rating of " + wardogs.ratings);
-}
-//MovieConstructor.prototype.listMovie();
-
 
 dataBase.pushMovie(new MovieConstructor("War Dogs", 2016, ["Comedy", " Crime", " Drama"]));
 console.log(dataBase.listMovies());
