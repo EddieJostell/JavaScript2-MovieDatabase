6f835ed43a6263bea8f39af4efcae68f73c103dd
Eddie <eddie.jostell@gmail.com>
2017-03-03 17:16:34 +0100

    added a background, posters for the movies and done some html

--- js/script2.js
+++ js/script2.js
@@ -9,6 +9,7 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Assassins Creed",
 		year: 2016,
+		cover: "https://s-media-cache-ak0.pinimg.com/originals/a2/90/da/a290dac56310aa3efbecfd4682f60ac2.png",
 		genres: ["Action", "Adventure", "Fantasy"],
 		ratings: [0,2,5,8,9,1,3,4,6,3]
 	},
@@ -16,6 +17,7 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Jason Bourne",
 		year: 2016,
+		cover: "http://s2.discshop.se/img/front_large/143147/jason_bourne.jpg",
 		genres: ["Action", "Thriller"],
 		ratings: [6,6,7,8,9,2,4,3,2,6]
 	},
@@ -23,6 +25,7 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Waterworld",
 		year: 1995,
+		cover: "https://images-na.ssl-images-amazon.com/images/I/51OovkfWLoL._SY300_.jpg",
 		genres: ["Action", "Adventure", "Sci-Fi"],
 		ratings: [8,9,6,5,7,8,6,7,5,9]
 	},
@@ -30,18 +33,21 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Blade Runner",
 		year: 1982,
+		cover: "https://images-na.ssl-images-amazon.com/images/I/51fHyCTQ2GL._SY300_.jpg",
 		genres: ["Sci-Fi", "Thriller"],
 		ratings: [9,8,9,7,9,8,9,8,9]
 	},
 	{
 		title: "The Fifth Element",
 		year: 1995,
+		cover: "https://images-na.ssl-images-amazon.com/images/I/51ZhOCOjPuL._SY300_.jpg",
 		genres: ["Action", "Adventure", "Sci-Fi"],
 		ratings: [9,9,9,9,9,9,10,8]
 	},
 	{
 		title: "Poltergeist",
 		year: 1982,
+		cover: "https://images-na.ssl-images-amazon.com/images/I/5151JIkQV9L._SY300_.jpg",
 		genres: ["Fantasy", "Horror", "Thriller"],
 		ratings: [9,9,9,9,7,8,6]
 	},
@@ -49,6 +55,7 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "The Big Lebowski",
 		year: 1998,
+		cover: "http://www.impawards.com/1998/posters/big_lebowski_ver1.jpg",
 		genres: ["Comedy", "Crime", "Mystery"],
 		ratings: [9,9,9,9,8,8,7,6,8,5]
 	},
@@ -56,12 +63,14 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Pulp Fiction",
 		year: 1994,
+		cover: "http://cdn.miramax.com/media/assets/Pulp-Fiction1.png",
 		genres: ["Crime", "Drama"],
 		ratings: [9,5,7,9,8,8,7,6,8,5]
 	},
 	{
 		title: "Titanic",
 		year: 1997,
+		cover: "https://s-media-cache-ak0.pinimg.com/736x/44/55/d9/4455d96357fb041d1cf3c8a5264ed593.jpg",
 		genres: ["Drama", "Romance"],
 		ratings: [7,6,8,5,7]
 	}, 
@@ -69,18 +78,21 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Fight Club",
 		year: 1999,
+		cover: "http://www.impawards.com/1999/posters/fight_club_ver3.jpg",
 		genres: ["Drama"],
 		ratings: [9,9,7,8,6,7,8,8,7,6] 
 	},
 	{
 		title: 'The Exorcist',
 		year: 1973,
+		cover: "http://www.impawards.com/1973/posters/exorcist.jpg",
 		genres: ['Horror','Thriller'],
 		ratings:[7,8,8,9]
 	},
 	{
 		title: 'Casino Royale',
 		year: 2006,
+		cover: "http://www.impawards.com/2006/posters/casino_royale.jpg",
 		genres: ['Action','Adventure','Thriller'],
 		ratings: [6,4,8,9,9,9,9]
 	},
@@ -88,6 +100,7 @@ const eddieMovieDatabase = (() => {
 	{
 		title:'Star Wars: Episode VI - Return of the Jedi',
 		year: 1983,
+		cover: "https://s-media-cache-ak0.pinimg.com/736x/82/bb/1a/82bb1a79c74ef439e7a93b5600373cdc.jpg",
 		genres:["Action", "Adventure", "Fantasy"],
 		ratings:[9,10,10,10,9]
 
@@ -95,12 +108,14 @@ const eddieMovieDatabase = (() => {
 	{
 		title: "Fury",
 		year: 2014,
+		cover: "http://www.impawards.com/2014/posters/fury.jpg",
 		genres: ["Action", "Drama", "War"],
 		ratings: [8,9,9,6,7,8,9,8]
 	},
 	{
 		title: "Gladiator",
 		year: 2000,
+		cover: "https://www.movieposter.com/posters/archive/main/42/MPW-21199",
 		genres: ["Action", "Adventure", "Drama"],
 		ratings: [10,10,10,10,10,10,10]
 	}
@@ -115,11 +130,12 @@ const eddieMovieDatabase = (() => {
 		//Den skapar objekt som då har prototyper, (javascripts sätt för att ärva egenskaper och metoder).
 		//Valde detta pattern istället för factory pattern för att jag inte har 
 		//behövt manipulera några prototyper direkt. 
-		MovieConstructor: function(title, year, genres, ratings) {
+		MovieConstructor: function(title, year, genres, ratings, cover) {
 			this.title = title;
 			this.year = year;
 			this.genres = genres.split(' ');
 			this.ratings = [ratings];
+			this.cover = cover;
 		},
         // Function that just returns the movies in the array.
         getMovies: () => {
@@ -131,8 +147,9 @@ const eddieMovieDatabase = (() => {
 		},
 		//Adds a movie typed in from the interface.
 		addMovieFromHTML: () => {
-			let addTitle = document.getElementById("title").value;
+			let addTitle = document.getElementById("title").value.trim();
 			let addYear = document.getElementById("year").value;
+			let addCover = document.getElementById("cover").value;
 			let addGenres = document.getElementById("genres").value;
 			let addRatings = document.getElementById("ratings").value;
 			let intRating = parseInt(addRatings);
@@ -145,12 +162,12 @@ const eddieMovieDatabase = (() => {
 			}
 			//If-statement that makes sure that a movie doesnt get added to the array
 			//if you leave a field empty.
-			if (addTitle === "" || addYear === "" || addGenres === "" || addRatings === "") {
+			if (addTitle === "" || addYear === "" || addGenres === "" || addRatings === "" || addCover === "") {
 				alert("All fields are required!");
 				return;
 			}
 			else {
-				let movieFromHTML = new eddieMovieDatabase.MovieConstructor(addTitle, addYear, addGenres, intRating);
+				let movieFromHTML = new eddieMovieDatabase.MovieConstructor(addTitle, addYear, addGenres, intRating, addCover);
 				eddieMovieDatabase.pushMovie(movieFromHTML);
 				eddieMovieDatabase.showMoviesOnHTML();
 				eddieMovieDatabase.populateEditDropDown();
@@ -168,10 +185,11 @@ const eddieMovieDatabase = (() => {
 			for (let i = 0; i < movies.length; i++) {
 				let rating = eddieMovieDatabase.movieRateCalculator(movies[i].ratings);
 				let blockofMovies = `<div class="movieDIV">
-				<p>Title : ${movies[i].title}</p>
-				<p>Release Year : ${movies[i].year}  </p>
-				<p>Genre(s) : ${movies[i].genres} </p>
-				<p>Rating : ${rating} </p>
+				<img width="200" height="300" src="${movies[i].cover}">
+				<h4>${movies[i].title}</h4>
+				<p>${movies[i].year}</p>				
+				<p>Genre(s): ${movies[i].genres} </p>
+				<p>Ratings: ${rating} </p>
 				</div>`;
 				movieUL.innerHTML += blockofMovies;
 			};
@@ -183,10 +201,11 @@ const eddieMovieDatabase = (() => {
 			for (let i = 0; i < x.length; i++) {
 				let rating = eddieMovieDatabase.movieRateCalculator(x[i].ratings);
 				let blockofMovies = `<div class="movieDIV">
-				<p>Title : ${x[i].title}</p>
-				<p>Release Year : ${x[i].year}  </p>
-				<p>Genre(s) : ${x[i].genres} </p>
-				<p>Rating : ${rating} </p>
+				<img width="200" height="300" src="${x[i].cover}">
+				<h4>${x[i].title}</h4>
+				<p>${x[i].year}  </p>
+				<p>Genre(s): ${x[i].genres} </p>
+				<p>Ratings: ${rating} </p>
 				</div>`;
 				movieUL.innerHTML += blockofMovies;
 			};
@@ -199,10 +218,11 @@ const eddieMovieDatabase = (() => {
 			
 			let rating = eddieMovieDatabase.movieRateCalculator(movies[index].ratings);
 			blockofMovies = `<div class="movieDIV">
-			<p>Title : ${movies[index].title}</p>
-			<p>Release Year : ${movies[index].year}  </p>
-			<p>Genre(s) : ${movies[index].genres} </p>
-			<p>Rating : ${rating} </p>
+			<img width="200" height="300" src="${movies[index].cover}">
+			<h4>${movies[index].title}</h4>
+			<p>${movies[index].year}  </p>
+			<p>Genre(s): ${movies[index].genres} </p>
+			<p>Ratings: ${rating} </p>
 			</div>`;
 			movieUL.innerHTML += blockofMovies;
 		},
@@ -213,10 +233,11 @@ const eddieMovieDatabase = (() => {
 			
 			let rating = eddieMovieDatabase.movieRateCalculator(movies[year].ratings);
 			blockofMovies = `<div class="movieDIV">
-			<p>Title : ${movies[year].title}</p>
-			<p>Release Year : ${movies[year].year}  </p>
-			<p>Genre(s) : ${movies[year].genres} </p>
-			<p>Rating : ${rating} </p>
+			<img width="200" height="300" src="${movies[year].cover}">
+			<h4>${movies[year].title}</h4>
+			<p>${movies[year].year}  </p>
+			<p>Genre(s): ${movies[year].genres} </p>
+			<p>Ratings: ${rating} </p>
 			</div>`;
 			movieUL.innerHTML += blockofMovies;
 			
@@ -244,14 +265,19 @@ const eddieMovieDatabase = (() => {
 		}, 
 		//Function that compares movieratings and lets you sort movies by highest and lowest rating.
 		sortByRating: (sortByHigh) => {
+			//Defined new empty array.
 			let sortedArray = [];
 
 			for (var i = 0; i < movies.length; i++) {
+				//Push excisting array into the new array.
 				sortedArray.push(movies[i]);
 			}
-              //Compare function that look at the calculated median rating value of the first movie in the array 
-              //and compare it against the value of the second movie.
-              //Depending
+              //Compare function that look at the calculated median rating value of a movie in the array 
+              //and compare it against the value of the another movie and returns a movie rating depending on largest value.
+              //If the boolean sortByHigh is not triggered indicating that the user want to sort by Lowest Rated movies.
+              //The built in sort(); function makes bubble sort on the values from the compareNumbers function and list the movies 
+              //with lowest rating first.
+              //If    
               let compareNumbers = (a, b) =>  {
               	var r1 = parseFloat(eddieMovieDatabase.movieRateCalculator(a.ratings));
               	var r2 = parseFloat(eddieMovieDatabase.movieRateCalculator(b.ratings));
@@ -336,6 +362,8 @@ const eddieMovieDatabase = (() => {
         			theMovie = movies[i];
         		}
         	}
+        	//If the genre you want to add is allready stated on the movie
+        	//it wont be added a second time.
         	for (let i = 0; i < theMovie.genres.length; i++) {
         		if (genreToAdd === theMovie.genres[i]){
         			return;
@@ -347,6 +375,8 @@ const eddieMovieDatabase = (() => {
         		alert("Please choose a genre!");
         		return;
         	}
+        	//Push the genre to the array of genres on the move choosen
+        	//and show it on the HTML.
         	else {
         		theMovie.genres.push(genreToAdd);
         		eddieMovieDatabase.showMoviesOnHTML();
